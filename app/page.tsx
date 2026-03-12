7f006a2bafbba58a30bc84ec4b8eac22c61f9ebb
Sam Sauer <sam.sauer@krekeny.com>
2026-03-12 12:30:47 +0100

    refator(Footer): add another blob for the footer background

--- app/page.tsx
+++ app/page.tsx
@@ -7,21 +7,27 @@ import StartingInOffenbach from "./components/StartingInOffenbach";
 import Waitlist from "./components/Waitlist";
 import FooterQuote from "./components/FooterQuote";
 import Footer from "./components/Footer";
+import GrainedBlob from "./components/GrainedBlob";
 
 export default function Home() {
   return (
     <>
       <Nav />
-      <main>
+      <main className="relative z-10">
         <Hero />
         <RememberWhen />
         <WeBelieve />
         <ValueCards />
         <StartingInOffenbach />
         <Waitlist />
-        <FooterQuote />
       </main>
-      <Footer />
+      <div className="relative z-0 overflow-clip pt-[500px] -mt-[500px]">
+        <GrainedBlob className="pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/8 rotate-55 z-0 w-[150%] h-auto" />
+        <div className="relative z-10">
+          <FooterQuote />
+          <Footer />
+        </div>
+      </div>
     </>
   );
 }
